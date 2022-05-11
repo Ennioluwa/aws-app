e313d320f168a4480497c02841febe281004075d
Your Name <enioluwaarowo@gmail.com>
2022-05-11 08:07:04 +0100

    created the individual links page

--- client/pages/admin/index.jsx
+++ client/pages/admin/index.jsx
@@ -1,17 +1,30 @@
 import axios from 'axios'
 import React from 'react'
 import AdminAuth from '../../components/auth/adminAuth'
+import Link from 'next/link'
 
-const Admin = ({ user }) => {
-  if (user) {
-    return (
-      <div>
-        <h3>Please authenticate</h3>
-      </div>
-    )
-  }
-  return <h3>hi</h3>
+const Admin = () => {
+  return (
+    <div className="container mx-auto flex flex-col gap-5 p-5">
+      <h1 className=" mb-4 text-4xl font-semibold">Admin Dashboard</h1>
+      <Link href={`/admin/category/create`}>
+        <a className=" text-blue-400">Create Category</a>
+      </Link>
+      <Link href={`/user/link/create`}>
+        <a className=" text-blue-400">Submit a Link</a>
+      </Link>
+      <Link href={`/admin/category/manage`}>
+        <a className=" text-orange-400">Manage Categories</a>
+      </Link>
+      <Link href={`/admin/links/manage`}>
+        <a className=" text-orange-400">Manage Links</a>
+      </Link>
+      <Link href={`/admin/profile/update`}>
+        <a className=" text-green-400">Update Profile</a>
+      </Link>
+    </div>
+  )
 }
 
 export default Admin
-export const getServerSideProps = AdminAuth(async (ctx) => {})
+export const getServerSideProps = AdminAuth()
